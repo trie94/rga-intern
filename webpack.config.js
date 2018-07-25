5aceac4dae38f4a3c7860223c36d8f071006a966
Yein Jo <yein.jo@rga.com>
2018-07-25 15:47:53 -0700

    set up server

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: ['./src/index.js'],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    
+    module:{
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                    options: { minimize: true }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'css-hot-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ],
+            },
+        ],
+    },
+
+    devtool: 'source-map',
+
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+            chunkFilename: '[id].css'
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        watchContentBase: true,
+        hot: true,
+        open: true,
+        port: 9000
+    }
+}
