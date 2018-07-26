4b3949572fc5d1cc326b517d0070c2b981a5d1c8
Yein Jo <yein.jo@rga.com>
2018-07-26 10:31:54 -0700

    fix bug on popup

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log("The color is green.");
   });
