4de7f237aded4c6aa51b33cc96eb0f8d79c6bfe5
Yein Jo <yein.jo@rga.com>
2018-07-31 10:01:05 -0700

    xml request and check the domain

--- background.js
+++ background.js
@@ -21,14 +21,18 @@ let data_pool;
 const user_url = chrome.runtime.getURL("src/data/userdata-template.js");
 let user_data = [];
 
-var xhr = new XMLHttpRequest();
+// request data
+let xhr = new XMLHttpRequest();
 xhr.open("GET", data_url, true);
 xhr.onreadystatechange = () => {
-  var resp = xhr.responseText;
-  console.log(resp);
+  if (xhr.readyState == 4 && xhr.status === 200) {
+    data_pool = JSON.parse(xhr.responseText);
+    // console.log(data_pool);
+  }
 }
 xhr.send();
 
+// test
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color: '#3aa757' }, () => {
     // console.log("value is set to", color);
@@ -44,40 +48,61 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+// when new tab created
 chrome.tabs.onCreated.addListener((tab) => {
 
   let url;
   let title;
   let platform;
 
+  // get the start time
   start_hour = date.getHours();
   start_minute = date.getMinutes();
 
-  fetch(data_url)
-    .then(response => { return response.json(); })
-    .then(data => {
-      for (let i = 0; i < Object.keys(data.domains).length; i++) {
-        for (let j = 0; j < data.domains[i].length; j++) {
-          if (tab.url.includes(data.domains[i][j])) {
-            platform = data.domains[i][0];
-            console.log(platform);
-            return platform;
-          }
-        }
+  // check the data pool
+  if (data_pool == null || data_pool == undefined) {
+    return;
+  } else {
+    console.log("data_pool: ", data_pool.domains);
+  }
+
+
+  for (let i = 0; i < Object.keys(data_pool.domains).length; i++) {
+    for (let j = 0; j < data_pool.domains[i].length; j++) {
+      if (tab.url.includes(data_pool.domains[i][j])) {
+        platform = data_pool.domains[i][0];
+        console.log(platform);
       }
-    });
+    }
+  }
+
+  // fetch(data_url)
+  //   .then(response => { return response.json(); })
+  //   .then(data => {
+  //     for (let i = 0; i < Object.keys(data.domains).length; i++) {
+  //       for (let j = 0; j < data.domains[i].length; j++) {
+  //         if (tab.url.includes(data.domains[i][j])) {
+  //           platform = data.domains[i][0];
+  //           console.log(platform);
+  //           console.log(typeof data);
+  //           return platform;
+  //         }
+  //       }
+  //     }
+  //   });
 
   console.log("created", tab.url, tab.title);
 });
 
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log("on updated", tab);
-})
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   console.log("on updated", tab);
+// });
+
 chrome.tabs.onRemoved.addListener(() => {
   end_hour = date.getHours();
   end_minute = date.getMinutes();
 
-  
+
   console.log("removed");
 });
