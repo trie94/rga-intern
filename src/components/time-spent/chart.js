3164a9b85b23111fd9faeec3f362c5dbdebdb872
Yein Jo <yein.jo@rga.com>
2018-08-06 09:16:44 -0700

    fix duplicates

--- src/components/time-spent/chart.js
+++ src/components/time-spent/chart.js
@@ -12,40 +12,42 @@ let top1_time = 0, top2_time = 0, top3_time = 0, top4_time = 0;
 console.log(profile_data);
 
 // profile_data.map((index) => { console.log(index.source) });
-// get top 4 sources
-
-top1_source = profile_data[0].source;
 let top_sources = [];
 
-for (let i = 0; i < profile_data.length; i++) {
-    top_sources[i] = profile_data[i].source;
-}
-
-function removeDuplicates(arr) {
-    let unique_array = arr.filter(function (elem, index, self) {
-        return index == self.indexOf(elem);
-    });
-    return unique_array;
+// get top 4 sources
+function getSources(){
+    for (let i = 0; i < profile_data.length; i++) {
+        top_sources[i] = profile_data[i].source;
+    }
+    console.log("get sources");
 }
 
 function filterSources(arr) {
+    top_sources = arr.filter(function (elem, index, self) {
+        return index == self.indexOf(elem);
+    });
+    console.log(top_sources);
+}
+
+function rankSources(arr) {
     if(top_sources[0] !== null || top_sources[0] !== undefined){
-        top1_source = top_sources[0];
+        top1_source = arr[0];
     }
     if(top_sources[1] !== null || top_sources[1] !== undefined){
-        top2_source = top_sources[1];
+        top2_source = arr[1];
     }
     if(top_sources[2] !== null || top_sources[2] !== undefined){
-        top3_source = top_sources[2];
+        top3_source = arr[2];
     }
-    if(top_sources[3] !== null || top_sources[3] !== undefined){
-        top4_source = top_sources[3];
-    }
+
+    console.log("top1_source: ", top1_source);
+    console.log("top2_source: ", top2_source);
+    console.log("top3_source: ", top3_source);
 };
 
-removeDuplicates(top_sources);
+getSources();
 filterSources(top_sources);
-console.log(removeDuplicates(top_sources));
+rankSources(top_sources);
 
 // get time
 for (let key of Object.keys(profile_data)) {
@@ -55,8 +57,6 @@ for (let key of Object.keys(profile_data)) {
         top2_time += profile_data[key].time;
     } else if (profile_data[key].source === top3_source) {
         top3_time += profile_data[key].time;
-    } else if (profile_data[key].source === top4_source) {
-        top4_time += profile_data[key].time;
     }
 }
 
@@ -70,9 +70,9 @@ class Chart extends React.Component {
         this.colors = [];
 
         this.sample_data = {
-            labels: [this.labels[0], this.labels[1], this.labels[2], this.labels[3]],
+            labels: [this.labels[0], this.labels[1], this.labels[2]],
             datasets: [{
-                data: [this.data[0], this.data[1], this.data[2], this.data[3]],
+                data: [this.data[0], this.data[1], this.data[2]],
                 backgroundColor: ["#e91e63", "#ff4136", "#b10dc9", "#06a6f3"],
                 hoverBackgroundColor: ["#e91e63", "#ff4136", "#b10dc9", "#06a6f3"]
             }]
