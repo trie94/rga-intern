a904042c0ed84af8c7f88b91a191cef6c3383fdd
Yein Jo <yein.jo@rga.com>
2018-08-08 11:35:57 -0700

    add persona buttons

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,57 +13,94 @@ class Header extends React.Component {
         this.weekAgo = new Date();
         this.calcDate = this.calcDate.bind(this);
         this.parseDate = this.parseMonth.bind(this);
+        this.refresh = this.refresh.bind(this);
+        this.getProfile = this.getProfile.bind(this);
+
+        this.buttonStyle = {
+            margin: "5px 5px 5px 0",
+            height: 30,
+            color: "#000",
+            backgroundColor: "#fff"
+        };
     }
 
-    calcDate(){
+    calcDate() {
         this.weekAgo.setDate(this.today.getDate() - 7);
-        console.log(this.weekAgo);
+        // console.log(this.weekAgo);
         return this.weekAgo;
     }
 
-    parseMonth(month_index){
-        
+    parseMonth(month_index) {
+
         let month = "";
 
-        switch (month_index){
+        switch (month_index) {
             case 0: month = "Jan"
-            break;
+                break;
             case 1: month = "Feb"
-            break;
+                break;
             case 2: month = "Mar"
-            break;
+                break;
             case 3: month = "Apr"
-            break;
+                break;
             case 4: month = "May"
-            break;
+                break;
             case 5: month = "June"
-            break;
+                break;
             case 6: month = "July"
-            break;
+                break;
             case 7: month = "Aug"
-            break;
+                break;
             case 8: month = "Sept"
-            break;
+                break;
             case 9: month = "Oct"
-            break;
+                break;
             case 10: month = "Nov"
-            break;
+                break;
             case 11: month = "Dec"
-            break;
+                break;
         }
 
         return month;
     }
 
+    refresh() {
+        console.log("do refresh");
+        location.reload();
+    };
+
+    getProfile() {
+        console.log("get profile");
+    }
+
     render() {
         return (
             <div>
+                <div className="personas component-container">
+                    <div>
+                        <button style={this.buttonStyle} onClick={this.refresh}>
+                            Get Another Report
+                    </button>
+                        <button style={this.buttonStyle} onClick={this.getProfile}>
+                            person1
+                    </button>
+                        <button style={this.buttonStyle} onClick={this.getProfile}>
+                            person2
+                    </button>
+                        <button style={this.buttonStyle} onClick={this.getProfile}>
+                            person3
+                    </button>
+                        <button style={this.buttonStyle} onClick={this.getProfile}>
+                            person4
+                    </button>
+                    </div>
+                </div>
                 <div className="logo component-container">
                     <img src={logo_colored} className="logo-img" />
                     <p>TILT WEEKLY REPORT</p>
                 </div>
                 <div className="date component-container">
-                <p>{this.parseMonth(this.calcDate().getMonth())} {this.calcDate().getDate()}, {this.calcDate().getFullYear()} - {this.parseMonth(this.today.getMonth())} {this.today.getDate()}, {this.today.getFullYear()}</p>
+                    <p>{this.parseMonth(this.calcDate().getMonth())} {this.calcDate().getDate()}, {this.calcDate().getFullYear()} - {this.parseMonth(this.today.getMonth())} {this.today.getDate()}, {this.today.getFullYear()}</p>
                 </div>
             </div>
         );
