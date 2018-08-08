c39658989905d4dbea650b9d016d97095cc3dc56
Yein Jo <yein.jo@rga.com>
2018-08-08 10:41:11 -0700

    add logo and date on the header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,70 @@
 import React from 'react';
 import './header.css';
+import logo from '../../assets/logo.png';
+import logo_colored from '../../assets/logo_colored.png';
+import logo_white from '../../assets/logo_white.png';
 
 class Header extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+
+        this.today = new Date();
+        this.weekAgo = new Date();
+        this.calcDate = this.calcDate.bind(this);
+        this.parseDate = this.parseMonth.bind(this);
+    }
+
+    calcDate(){
+        this.weekAgo.setDate(this.today.getDate() - 7);
+        console.log(this.weekAgo);
+        return this.weekAgo;
+    }
+
+    parseMonth(month_index){
+        
+        let month = "";
+
+        switch (month_index){
+            case 0: month = "Jan"
+            break;
+            case 1: month = "Feb"
+            break;
+            case 2: month = "Mar"
+            break;
+            case 3: month = "Apr"
+            break;
+            case 4: month = "May"
+            break;
+            case 5: month = "June"
+            break;
+            case 6: month = "July"
+            break;
+            case 7: month = "Aug"
+            break;
+            case 8: month = "Sept"
+            break;
+            case 9: month = "Oct"
+            break;
+            case 10: month = "Nov"
+            break;
+            case 11: month = "Dec"
+            break;
+        }
+
+        return month;
     }
 
     render() {
         return (
-            <div className="header component-container">
-                Header
+            <div>
+                <div className="logo component-container">
+                    <img src={logo_colored} className="logo-img" />
+                    <p>TILT WEEKLY REPORT</p>
+                </div>
+                <div className="date component-container">
+                <p>{this.parseMonth(this.calcDate().getMonth())} {this.calcDate().getDate()}, {this.calcDate().getFullYear()} - {this.parseMonth(this.today.getMonth())} {this.today.getDate()}, {this.today.getFullYear()}</p>
+                </div>
             </div>
         );
     }
