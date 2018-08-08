f1f55f91624af050ea2579303f9b1d2c4851a992
trie94 <trie60@gmail.com>
2018-08-07 23:03:19 -0700

    update

--- src/data/user-data.js
+++ src/data/user-data.js
@@ -1,8 +1,8 @@
 import { generateProfile } from '../js/generative-profile-system';
 import { getAvgScore, getTotalTime, getReport } from '../js/report';
 
-let bias = "left";
-let cred = "low";
+let bias = "center";
+let cred = "high";
 let totalNum = 20;
 let user = generateProfile(bias, cred, totalNum, 5);
 let score = getAvgScore(user);
